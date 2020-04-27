86ce294a427c938446f153d735258959475c5198
Matt Heerspink <mheerspink75@gmail.com>
2020-04-27 00:25:13 -0500

    updated index.js

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require('firebase-functions');
+const firebaseUser = require('./firebaseUser');
 
-
+const createError = require('http-errors');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const firebaseUser = require('./firebaseUser');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -15,6 +15,22 @@ app.get('/', (req, res) => {
   return res.render('user', {
     user: req.user,
   });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 exports.app = functions.https.onRequest(app);
