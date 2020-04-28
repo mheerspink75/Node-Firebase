f3b2670ff6b90cdff6c3c7e15ee2a8ebfbb8daf3
Matt Heerspink <mheerspink75@gmail.com>
2020-04-27 21:01:17 -0500

    updated user.handlebars

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require('firebase-functions');
 const firebaseUser = require('./firebaseUser');
+const admin = require("firebase-admin");
+const db = admin.firestore();
 
 const createError = require('http-errors');
 const express = require('express');
 const exphbs = require('express-handlebars');
+
 const app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -12,10 +15,13 @@ app.use(firebaseUser.validateFirebaseIdToken);
 
 app.get('/', (req, res) => {
   console.log('Signed-in user:', req.user);
+  console.log(db.collection('users').id);
   return res.render('user', {
     user: req.user,
+    users: db.collection('users').id
   });
 });
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
